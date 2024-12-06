545d742f682076e318090ef01c42589ce23ac752
Vignesh .M <149869418+vigneshkira@users.noreply.github.com>
2024-12-06 21:35:02 +0530

    Update kitchen_node.py

--- cafe_root/kitchen_node.py
+++ cafe_root/kitchen_node.py
@@ -6,7 +6,7 @@ import time
 class KitchenNode(Node):
     def __init__(self):
         super().__init__('kitchen_node')
-        # Subscription to receive orders from the Butler (i.e., customer order)
+        
         self.order_sub = self.create_subscription(
             String,
             'customer/order',  # The topic from the Butler
@@ -14,27 +14,25 @@ class KitchenNode(Node):
             10
         )
 
-        # Publisher to notify that food is ready
+        
         self.ready_pub = self.create_publisher(
             String,
-            'kitchen/ready',  # Topic where kitchen will notify that food is ready
+            'kitchen/ready', 
             10
         )
 
         self.get_logger().info('Kitchen Node started and waiting for orders.')
 
     def handle_order(self, msg):
-        """
-        Handles incoming orders from the Butler.
-        """
+        
         order = msg.data
         self.get_logger().info(f"Received order: {order}")
         
-        # Simulate food preparation
+        
         self.get_logger().info("Preparing the food...")
         time.sleep(5)  # Simulating cooking time (5 seconds)
 
-        # Send a confirmation back that the food is ready
+       
         ready_msg = String()
         ready_msg.data = f"Food for {order} is ready!"
         self.ready_pub.publish(ready_msg)
